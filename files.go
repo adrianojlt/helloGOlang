50bee7877dc151eac64ac40ae1e65fe412bcc6bd
Adriano Teixeira <adrianojlt@gmail.com>
2017-03-19 00:17:33 +0000

    list files in dir

--- files.go
+++ files.go
@@ -1,7 +1,20 @@
 package main
 
 import "fmt"
+import "log"
+import iou "io/ioutil"
 
 func main() {
-	
+
+	fmt.Println("List of files: ")
+
+	files, err := iou.ReadDir("testdir")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
