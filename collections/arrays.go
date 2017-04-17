f5f0000ff6d1625fc924bda13f767b9170fa01ae
Adriano Teixeira <adrianojlt@gmail.com>
2017-04-17 18:08:32 +0100

    new folders

---
+++ collections/arrays.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+}
+
+func main() {
+	var array [5] int
+}
+
+func arrays() {
+
+	var john User
+	bill := new(User)
+	bill.name = "BILL"
+	fmt.Println(john.enabled)
+	fmt.Println(bill.name)
+
+	var initArray = []string{"2", "3"}
+	fmt.Println(initArray)
+}
