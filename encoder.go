d425032f3510e1b5d8d5d96b2193c3bfcf64e1c0
Adriano Teixeira <adrianojlt@gmail.com>
2017-04-27 00:25:24 +0100

    walk through dir

--- encoder.go
+++ encoder.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+
 	"golang.org/x/text/encoding/charmap"
 )
+
+var extensions = []string{"srt", "sub", "txt"}
 
 func create() {
 	iFile, err := os.Open("isotext.txt")
@@ -21,36 +27,54 @@ func create() {
 	}
 
 	r := charmap.ISO8859_1.NewDecoder().Reader(iFile)
-	io.Copy(oFile,r)
+	io.Copy(oFile, r)
 
-	oFile.Close();
+	oFile.Close()
 	iFile.Close()
 }
 
-func BOMit(filePath string) {
+func bomIt(filePath string, showContent bool) {
+
 	// Open a file.
-    f, _ := os.Open(filePath)
+	f, _ := os.Open(filePath)
 
-    // Use bufio.NewReader to get a Reader.
-    // ... Then use ioutil.ReadAll to read the entire content.
-    reader := bufio.NewReader(f)
-    content, _ := ioutil.ReadAll(reader)
+	// Use bufio.NewReader to get a Reader.
+	// ... Then use ioutil.ReadAll to read the entire content.
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
 
-    // The UTF-8 BOM is a sequence of bytes (EF BB BF) that allows the reader to identify a file as being encoded in UTF-8.
-    // The byte order mark (BOM) is a Unicode character, U+FEFF
-    // at the start of a text stream can signal several things to a program consuming the text
-    total := "\uFEFF" + string(content)
-    f.Close()
+	// The UTF-8 BOM is a sequence of bytes (EF BB BF) that allows the reader to identify a file as being encoded in UTF-8.
+	// The byte order mark (BOM) is a Unicode character, U+FEFF
+	// at the start of a text stream can signal several things to a program consuming the text
+	total := "\uFEFF" + string(content)
+	f.Close()
 
-    // Print File content.
-    fmt.Println(string(content))
+	// Print File content.
+	fmt.Println(string(content))
 
-    // write ...
-    ioutil.WriteFile(filePath, []byte(total), 0644)
+	// write ...
+	ioutil.WriteFile(filePath, []byte(total), 0644)
+}
+
+func walker(path string, f os.FileInfo, err error) error {
+
+	if !f.IsDir() && strings.HasSuffix(f.Name(), "srt") {
+		bomIt(path, true)
+	}
+
+	return nil
 }
 
 func main() {
-	//create()
-	BOMit("test.txt")
-}
 
+	//create()
+
+	flag.Parse()
+	root := flag.Arg(0)
+	err := filepath.Walk(root, walker)
+	if err != nil {
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
+
+	//bomIt("test.txt", false)
+}
--- functions.go
+++ functions.go
@@ -13,28 +13,35 @@ func add(a int, b int) int {
 }
 
 func power(name string) (int, bool) {
-	return 0,true;
+	return 0, true
 }
 
 func anonymousFunctions() {
 
 	// anonymous functions with different type returns
-	a,b := func(a,b int,s string) (int,int) {
-		return 2,3
-	}(1,2,"string")
+	a, b := func(a, b int, s string) (int, int) {
+		return 2, 3
+	}(1, 2, "string")
 
-	a,b = func(a bool,b int) (int,int) {
-		return 2,3
-	}(true,120)
+	a, b = func(a bool, b int) (int, int) {
+		return 2, 3
+	}(true, 120)
 
-	fmt.Printf("%d , %d\n",a,b)
+	fmt.Printf("%d , %d\n", a, b)
 }
 
+// named return values
+func rectProps(length, width float64) (area, perimeter float64) {
+	area = length * width
+	perimeter = (length + width) * 2
+	return //no explicit return value
+}
 
 func main() {
 	_, exists := power("cenadas")
 
-	if ( exists ) {}
+	if exists {
+	}
 
 	func() {
 		fmt.Println("anonymous function")
