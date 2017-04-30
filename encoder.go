ed714bf31f4c01d5ad203f01314787977d77fc7d
Adriano Teixeira <adrianojlt@gmail.com>
2017-04-30 10:40:56 +0100

    bom in utf files

--- encoder.go
+++ encoder.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/saintfish/chardet"
 	"golang.org/x/text/encoding/charmap"
 )
 
@@ -35,24 +36,35 @@ func create() {
 
 func bomIt(filePath string, showContent bool) {
 
+	var convertedContent []byte
+
 	// Open a file.
 	f, _ := os.Open(filePath)
 
 	// Use bufio.NewReader to get a Reader.
 	// ... Then use ioutil.ReadAll to read the entire content.
 	reader := bufio.NewReader(f)
-	content, _ := ioutil.ReadAll(reader)
+	originalContent, _ := ioutil.ReadAll(reader)
 
-	// The UTF-8 BOM is a sequence of bytes (EF BB BF) that allows the reader to identify a file as being encoded in UTF-8.
+	// try to detect the encoding used
+	detector := chardet.NewTextDetector()
+	result, _ := detector.DetectBest(originalContent)
+	charset := result.Charset
+
+	if strings.Contains(charset, "UTF") {
+		convertedContent = originalContent
+	} else {
+		// now ... here i am assuming ISO08859 but i need to use the one found in the previous detect encoding operation
+		convertedContent, _ = charmap.ISO8859_1.NewDecoder().Bytes(originalContent)
+	}
+
+	// The UTF-8 BOM (Byte Order Mark) is a sequence of bytes (EF BB BF) that allows the reader to identify a file as being encoded in UTF-8.
 	// The byte order mark (BOM) is a Unicode character, U+FEFF
 	// at the start of a text stream can signal several things to a program consuming the text
-	total := "\uFEFF" + string(content)
+	total := "\uFEFF" + string(convertedContent)
 	f.Close()
 
-	// Print File content.
-	fmt.Println(string(content))
-
-	// write ...
+	// write (default encoding in golang is UTF-8) ...
 	ioutil.WriteFile(filePath, []byte(total), 0644)
 }
 
@@ -65,16 +77,52 @@ func walker(path string, f os.FileInfo, err error) error {
 	return nil
 }
 
-func main() {
+func list(path string, f os.FileInfo, err error) error {
 
-	//create()
+	if !f.IsDir() && strings.HasSuffix(f.Name(), "srt") {
+		fmt.Println(f.Name(), "\t encoded with: ", detect(path))
+	}
 
+	return nil
+}
+
+func detect(path string) string {
+	f, _ := os.Open(path)
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
+	detector := chardet.NewTextDetector()
+	result, _ := detector.DetectBest([]byte(content))
+	return result.Charset
+}
+
+func start() {
 	flag.Parse()
 	root := flag.Arg(0)
 	err := filepath.Walk(root, walker)
 	if err != nil {
 		fmt.Printf("filepath.Walk() returned %v\n", err)
 	}
+}
 
-	//bomIt("test.txt", false)
+func main() {
+	//create()
+	start()
+	//test()
+	//bomIt("./testdir/iso8859.srt", false)
+}
+
+func test() {
+	flag.Parse()
+	folder := flag.Arg(0)
+	_ = filepath.Walk(folder, list)
+	//name := charmap.ISO8859_1.String()
+}
+
+/*
+try this
+boomit --dir="./dir"
+boomit --dir="c:\temp\subtitles" -x
+*/
+func args() {
+
 }
---
+++ strings.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+
+func main() {
+	fmt.Println(sample)
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("%x ", sample[i])
+	}
+	fmt.Printf("\n%x\n", sample)
+	fmt.Printf("% x\n", sample)
+}
