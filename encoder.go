8f5ee506544c9c86415c3adcd09187c0ac2b30a2
Adriano Teixeira <adrianojlt@gmail.com>
2017-06-04 01:46:41 +0100

    refractored encoding change

--- encoder.go
+++ encoder.go
@@ -34,6 +34,48 @@ func create() {
 	iFile.Close()
 }
 
+func readFile(path string) []byte {
+	// Open a file.
+	f, _ := os.Open(path)
+
+	// Use bufio.NewReader to get a Reader.
+	// ... Then use ioutil.ReadAll to read the entire content.
+	reader := bufio.NewReader(f)
+	originalContent, _ := ioutil.ReadAll(reader)
+
+	f.Close()
+	return originalContent
+}
+
+func convert(originalContent []byte) []byte {
+	var convertedContent []byte
+
+	// try to detect the encoding used
+	detector := chardet.NewTextDetector()
+	result, _ := detector.DetectBest(originalContent)
+	charset := result.Charset
+
+	if strings.Contains(charset, "UTF") {
+		convertedContent = originalContent
+	} else {
+		// now ... here i am assuming ISO08859 but i need to use the one found in the previous detect encoding operation
+		convertedContent, _ = charmap.ISO8859_1.NewDecoder().Bytes(originalContent)
+	}
+
+	return convertedContent
+}
+
+func boomIT(content []byte) {
+
+	// The UTF-8 BOM (Byte Order Mark) is a sequence of bytes (EF BB BF) that allows the reader to identify a file as being encoded in UTF-8.
+	// The byte order mark (BOM) is a Unicode character, U+FEFF
+	// at the start of a text stream can signal several things to a program consuming the text
+	total := "\uFEFF" + string(convertedContent)
+
+	// write (default encoding in golang is UTF-8) ...
+	ioutil.WriteFile(filePath, []byte(total), 0644)
+}
+
 func bomIt(filePath string, showContent bool) {
 
 	var convertedContent []byte
@@ -71,7 +113,8 @@ func bomIt(filePath string, showContent bool) {
 func walker(path string, f os.FileInfo, err error) error {
 
 	if !f.IsDir() && strings.HasSuffix(f.Name(), "srt") {
-		bomIt(path, true)
+		boomIT(convert(readFile(path)))
+		//bomIt(path, true)
 	}
 
 	return nil
@@ -97,8 +140,8 @@ func detect(path string) string {
 
 func start() {
 	flag.Parse()
-	root := flag.Arg(0)
-	err := filepath.Walk(root, walker)
+	path := flag.Arg(0)
+	err := filepath.Walk(path, walker)
 	if err != nil {
 		fmt.Printf("filepath.Walk() returned %v\n", err)
 	}
